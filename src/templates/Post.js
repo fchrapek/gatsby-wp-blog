2cacc24f936801caed461cfbd33288ae73046d3f
Filip Chrapek <hello@filipchrapek.com>
2022-12-14 02:03:10 +0100

    Add support for ACF flexible sections

--- src/templates/Post.js
+++ src/templates/Post.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { graphql } from "gatsby";
 import { GatsbyImage } from "gatsby-plugin-image";
-import Test from "../components/Test";
 import SEO from "../components/SEO";
 import parse from 'html-react-parser';
 
@@ -16,7 +15,9 @@ export default function SinglePostPage({ data: { post } }) {
     <>
       <SEO title={post.title} />
 
-      <div>
+      <article>
+        <h1>{post.title}</h1>
+
         <p>{post.terms.nodes.map(post => (post.name)).join(', ')}</p>
 
         {featuredImage?.data && (
@@ -26,13 +27,36 @@ export default function SinglePostPage({ data: { post } }) {
           />
         )}
 
-        {flexibleContent?.map(data => (
-          <React.Fragment key={Math.floor(Math.random() * 100)}>
-            {data?.content && parse(data.content)}
-          </React.Fragment>
-        ))
-        }
-      </div>
+        {flexibleContent?.map(data => {
+          const image = {
+            data: data.image?.localFile?.childImageSharp?.gatsbyImageData,
+            alt: data.image?.alt || ``,
+          }
+
+          return (
+            <React.Fragment key={Math.floor(Math.random() * 100)}>
+              {data?.content && parse(data.content)}
+
+              {image?.data && (
+                <GatsbyImage
+                  image={image.data}
+                  alt={image.alt}
+                />
+              )}
+
+              {data.section?.map(sectionData => {
+                return (
+                  <section key={Math.floor(Math.random() * 100)}>
+                    {sectionData.flexibleContent.map(flexibleContent => (
+                      flexibleContent?.content && parse(flexibleContent.content)
+                    ))}
+                  </section>
+                )
+              })}
+            </React.Fragment>
+          )
+        })}
+      </article>
     </>
   )
 }
@@ -57,7 +81,30 @@ export const query = graphql`
         }
       }
       post {
-          flexibleContent {
+        flexibleContent {
+          ... on WpPost_Post_FlexibleContent_Section {
+            section {
+              flexibleContent {
+                ... on WpPost_Post_FlexibleContent_Section_section_FlexibleContent_Content {
+                  content
+                }
+                ... on WpPost_Post_FlexibleContent_Section_section_FlexibleContent_Image {
+                  image {
+                    altText
+                    localFile {
+                      childImageSharp {
+                        gatsbyImageData(
+                          quality: 100
+                          placeholder: DOMINANT_COLOR
+                          layout: FULL_WIDTH
+                        )
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
           ... on WpPost_Post_FlexibleContent_Content {
             content
           }
